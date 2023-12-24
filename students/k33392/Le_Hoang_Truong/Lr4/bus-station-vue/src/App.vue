5796d2bdd08b2d9a6abe06df32dfeba5c6844bc2
depchai1511 <natibatian@gmail.com>
2023-12-25 01:01:30 +0300

    feat : add lab 4

---
+++ students/k33392/Le_Hoang_Truong/Lr4/bus-station-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <NavBar></NavBar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from "../src/components/NavBar.vue"
+
+export default {
+  name: 'App',
+  
+  components: {
+    NavBar,
+  },
+ 
+
+}
+
+</script>
+
+<style>
+
+@import '@/assets/main.css';
+@import '@/assets/light.css';
+@import '@/assets/dark.css';
+</style>
